fa1490b14bb54b5989fdf7940bc633408a0bc611
huynhvantu <huynhvantunlp@gmail.com>
2019-05-21 23:50:16 +0700

    first commit

--- bai3javascript.js
+++ bai3javascript.js
@@ -2,5 +2,5 @@ function xuly(){
 	console.log('da vao ham...')
 	var bankinh= document.getElementById('bankinh').value;
 	var dientich= (parseInt (bankinh)*parseInt(bankinh))*3.14;
-	document.getElementById('dientich').innerHTML='Dien tich la:' +dientich;
+	document.getElementById('dientich').innerHTML='Dien tich hinh tron la:' +dientich;
 }
