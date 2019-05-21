b6b18862d23c3777bdad4d75c39453b4a33db09f
huynhvantu <huynhvantunlp@gmail.com>
2019-05-21 23:57:29 +0700

    first commit

---
+++ bai4javascript.js
@@ -0,0 +1,6 @@
+function xuly(){
+	console.log('da vao ham...')
+	var bankinh= document.getElementById('bankinh').value;
+	var chuvi= (parseInt (bankinh))*2*3.14;
+	document.getElementById('chuvi').innerHTML='Chu vi hinh tron la:' +chuvi;
+}
